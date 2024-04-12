5bf34d9a9c914adddf4ded6d6d9c3dc293a39d16
Mateo <dzmateozd@gmail.com>
2024-04-12 11:39:47 -0300

    Ejercicio 2

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
-    print("TO DO")
+    y = int(input("Ingrese un año: "))
+
+    d4 = not y % 4      # Es divisible por 4?
+    d100 = not y % 100  # Es divisible por 100?
+    d400 = not y % 400  # Es divisible por 400?
+
+    # Si es div. por 4 pero no centenario ó div. por 400
+    if (d4 and not d100) or d400:
+        print(f"El año {y} es bisiesto")
+    else:
+        print(f"El año {y} no es bisiesto")
